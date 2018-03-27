50924a7e68be01e635095c85ef53a0da92d13273
HowTommy <howtommy.net@gmail.com>
2018-03-28 00:55:49 +0200

    Locking pieces + moving them + falling

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        SpawnNextTetromino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool CheckIsInsideGrid(Vector2 position)
+    {
+        return ((int)position.x >= 1 && (int)position.x - 1 < gridWidth && (int)position.y > 0);
+    }
+
+    public Vector2 Round(Vector2 position)
+    {
+        return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
+    }
+
+    public void SpawnNextTetromino()
+    {
+        GameObject nextTetromino = (GameObject)Instantiate(Resources.Load("Prefabs/" + GetRandomTetromino(), typeof(GameObject)), new Vector2(5, 20), Quaternion.identity);
+    }
+
+    string GetRandomTetromino()
+    {
+        var randomTetrominoId = Random.Range(1, 8);
+        switch(randomTetrominoId)
+        {
+            case 1:
+                return "Tetromino_T";
+            case 2:
+                return "Tetromino_Z";
+            case 3:
+                return "Tetromino_Square";
+            case 4:
+                return "Tetromino_S";
+            case 5:
+                return "Tetromino_Long";
+            case 6:
+                return "Tetromino_L";
+            case 7:
+                return "Tetromino_J";
+        }
+
+        throw new System.Exception();
+    }
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,16 +1,125 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tetromino : MonoBehaviour {
+public class Tetromino : MonoBehaviour
+{
+    float fall = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float fallSpeed = 1;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUserInput();
+    }
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveTetrominoIfAllowed(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveTetrominoIfAllowed(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveTetrominoIfAllowed(0, -1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            RotateTetrominoIfAllowed();
+        }
+
+        if (Time.time - fall >= fallSpeed)
+        {
+            MoveTetrominoIfAllowed(0, -1, 0);
+            fall = Time.time;
+        }
+    }
+
+    private void RotateTetrominoIfAllowed()
+    {
+        if(!allowRotation)
+        {
+            return;
+        }
+
+        if (!limitRotation)
+        {
+            transform.Rotate(0, 0, -90);
+
+            if (!CheckIsValidPosition())
+            {
+                transform.Rotate(0, 0, 90);
+            }
+            return;
+        }
+
+        var currentRotationZ = transform.rotation.eulerAngles.z;
+        if (currentRotationZ >= 90)
+        {
+            transform.Rotate(0, 0, -currentRotationZ);
+
+            if(!CheckIsValidPosition())
+            {
+                transform.Rotate(0, 0, currentRotationZ);
+            }
+        }
+        else
+        {
+            transform.Rotate(0, 0, 90);
+
+            if (!CheckIsValidPosition())
+            {
+                transform.Rotate(0, 0, -90);
+            }
+        }
+    }
+
+    private void MoveTetrominoIfAllowed(float x, float y, float z)
+    {
+        transform.position += new Vector3(x, y, z);
+
+        if (!CheckIsValidPosition())
+        {
+            transform.position += new Vector3(-x, -y, -z);
+
+            // if we try to move down that piece, we disable it + spawn new tetromino
+            if (x == 0 && z == 0 && y < 0)
+            {
+                enabled = false;
+                FindObjectOfType<Game>().SpawnNextTetromino();
+            }
+        }
+
+
+    }
+
+    bool CheckIsValidPosition()
+    {
+        var gameScript = FindObjectOfType<Game>();
+        foreach (Transform tetromino in transform)
+        {
+            Vector2 roundedPosition = gameScript.Round(tetromino.position);
+            if (gameScript.CheckIsInsideGrid(roundedPosition) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
